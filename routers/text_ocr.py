d50177b99a13a93a14cc56f8ea0c3c959ebab3df
Lepilov <evgenylepilov@gmail.com>
2020-07-20 21:50:32 +0300

    add logic for static template form to file upload

--- routers/text_ocr.py
+++ routers/text_ocr.py
@@ -1,5 +1,7 @@
-from fastapi import HTTPException, APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, File
 from crud.text_ocr import create_upload_file
+from fastapi.responses import HTMLResponse
+
 router = APIRouter()
 
 
@@ -7,3 +9,20 @@ router = APIRouter()
 async def file_upload(file: UploadFile = File(...)):
     response = await create_upload_file(file)
     return response
+
+
+# @router.get("/file/")
+# async def file_get():
+#     return HTMLResponse(html)
+
+@router.get("/file/")
+async def file_receive():
+    content = """
+<body>
+<form action = "http://localhost:8000/image/uploadfile/" enctype="multipart/form-data" method="post">
+<input name="file" type="file" multiple>
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
